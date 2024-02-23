cb2285fa244bd2322e47198aa87a2f8d5a77fe84
ta4toDev <irina.jaudszims@gmail.com>
2024-02-23 10:58:09 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static String repeatStr(String str, int count) {
+        return str.repeat(count);
+    }
+    public static void main(String[] args) {
+        System.out.println(repeatStr("hallo ",7));
+    }
+}
